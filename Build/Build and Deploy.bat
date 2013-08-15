a7c9afde1dcdfd962a6cd2f2a8c99421d7efefee
Simon Colmer <simon_colmer@hotmail.com>
2013-08-15 23:07:56 +0100

    Added nuget stuff

---
+++ Build/Build and Deploy.bat
@@ -0,0 +1,2 @@
+nuget pack ..\LocalDbApi\LocalDbApi.csproj -Prop Configuration=Release
+nuget push LocalDbApi.1.0.0.*.nupkg
--- LocalDbApi/Properties/AssemblyInfo.cs
+++ LocalDbApi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LocalDbApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(@"A small wrapper for LocalDb - using command line stuff. Can be used for creating and maintaining LocalDb instances. Super light and really simple - just a nice wrapper...")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Simon Colmer")]
 [assembly: AssemblyProduct("LocalDbApi")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © Simon Colmer 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
